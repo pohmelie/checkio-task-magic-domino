55e6d97e29e95fe0c30226061322c493a3bc74b8
pohmelie <multisosnooley@gmail.com>
2014-07-08 10:26:54 +0400

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -1,49 +1,81 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
+import itertools
 
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
 
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
+TYPE_ERROR = False, {"error_code": 1, "message": "You should return a list/tuple of lists/tuples with integers."}
+SIZE_ERROR = False, {"error_code": 2, "message": "Wrong size of answer."}
+MAGIC_ERROR = False, {"error_code": 3, "message": "It's not a magic square."}
+DOMINO_ERROR = False, {"error_code": 4, "message": "It's not a domino magic square."}
+ALL_OK = True, {"error_code": 100, "message": "All ok."}
 
-"""
+def check_data(size, number, user_result):
+
+    #check types
+    check_container_type = lambda o: any(map(lambda t: isinstance(o, t), (list, tuple)))
+    check_cell_type = lambda i: isinstance(i, int)
+    if not (
+        check_container_type(user_result) and
+        all(map(check_container_type, user_result)) and
+        all(map(lambda row: all(map(check_cell_type, row)), user_result))
+    ):
+
+        return TYPE_ERROR
+
+    #check sizes
+    check_size = lambda o: len(o) == size
+    if not (check_size(user_result) and all(map(check_size, user_result))):
+
+        return SIZE_ERROR
+
+    #check is it a magic square
+    seq_sum_check = lambda seq: sum(seq) == number
+    diagonals_indexes = zip(*map(lambda i: ((i, i), (i, size - i - 1)), range(size)))
+    values_from_indexes = lambda inds: itertools.starmap(lambda x, y: user_result[y][x], inds)
+    if not (
+        all(map(seq_sum_check, user_result)) and                              # rows
+        all(map(seq_sum_check, zip(*user_result))) and                        # columns
+        all(map(seq_sum_check, map(values_from_indexes, diagonals_indexes)))  # diagonals
+    ):
+
+        return MAGIC_ERROR
+
+    #check is it domino square
+    tiles = set()
+    for x, y in itertools.product(range(size), range(0, size, 2)):
+
+        tile = tuple(sorted((user_result[y][x], user_result[y + 1][x])))
+        if tile in tiles:
+
+            return DOMINO_ERROR
+
+        tiles.add(tile)
+
+
+    return ALL_OK
+
 
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
+
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
         cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
+            'python-27': None,
+            'python-3': None
         },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
-    ).on_ready)
+        checker=check_data
+    ).on_ready
+)
+
+'''
+print(check_data(4, 5, set()))
+print(check_data(4, 5, [(),]))
+print(check_data(4, 5, [(0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)]))
+print(check_data(4, 4, [(1, 1, 1, 1), (1, 1, 1, 1), (1, 1, 1, 1), (1, 1, 1, 1)]))
+print(check_data(4, 5, [(0, 0, 2, 3), (0, 4, 1, 0), (4, 0, 0, 1), (1, 1, 2, 1)]))
+'''
