3952d2af13ef9c6f910ecab3cb9ac3184b92e1b9
pohmelie <multisosnooley@gmail.com>
2014-07-08 10:42:08 +0400

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -1,36 +1,19 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
-"""
+import random
 
+
+extra_tests_count = 3
 
 TESTS = {
-    "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    "Basics": [],
+    "Extra": [],
 }
+
+
+for n in range(5, 20):
+
+    TESTS["Basics"].append({"input": (4, n), "answer": (4, n)})
+
+
+for n in random.sample(range(13, 24), extra_tests_count):
+
+    TESTS["Extra"].append({"input": (6, n), "answer": (6, n)})
